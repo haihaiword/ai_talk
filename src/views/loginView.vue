09d5110caa1040c62594d9ba57aebcb4e073ddf6
琳 <2147721563@qq.com>
2024-01-28 22:55:59 +0800

    安装饿了么ui框架+login登录页

--- src/views/loginView.vue
+++ src/views/loginView.vue
@@ -1,45 +1,94 @@
+<template>
+  <div id="login">
+    <div class="login-box">
+      <!-- 标题 -->
+      <div class="title">
+        <h1>AI 复盘登录</h1>
+      </div>
+      <!-- 表单 -->
+      <el-form>
+        <el-form-item>
+          <el-input class="input" placeholder="账号" :prefix-icon="User"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input class="input" placeholder="密码" :prefix-icon="Lock"></el-input>
+        </el-form-item>
+
+        <div class="btns">
+          <el-button type="primary" round @click="toLogin" class="btn">登录</el-button>
+          <div class="reset">重置</div>
+        </div>
+      </el-form>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
+import { User, Lock } from '@element-plus/icons-vue'
 import { useRouter } from 'vue-router';
 const router = useRouter();
 
 // 登录(简易版)
 const toLogin = () => {
-  router.replace('/home')
+  router.push('/home')
 }
 </script>
 
-<template>
-  <div class="login-content">
-    <div class="login">
-      <div class="login-title">
-        登录
-      </div>
-
-      <input type="text" placeholder="账号">
-      <input type="text" placeholder="密码">
-
-      <button @click="toLogin">登录</button>
-    </div>
-  </div>
-</template>
-
 <style lang="scss" scoped>
-.login-content {
+#login {
+  height: 100vh;
+  background: linear-gradient(to right, #2b2c43, #1e3158);
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
 
-  .login {
-    width: 400px;
-    height: 500px;
-    border: 1px solid rgb(253, 169, 169);
-    text-align: center;
+  .login-box {
+    width: 360px;
+    height: 450px;
+    background-color: #fff;
+    border-radius: 10px;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    align-items: center;
     padding: 20px;
-    box-sizing: content-box;
+
+    .title {
+      height: 120px;
+      line-height: 120px;
+    }
+
+    .el-form {
+      width: 90%;
+      margin-top: 30px;
+
+      .input {
+        height: 40px;
+
+        :deep(.el-input__wrapper){
+          border-radius: 20px;
+          // border: 1px solid #000;
+        }
+      }
+
+      .btns {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .btn {
+          width: 100%;
+          height: 40px;
+          border-radius: 20px;
+          margin-bottom: 20px;
+          cursor: pointer;
+        }
+
+        .reset {
+          text-align: center;
+          cursor: pointer;
+        }
+      }
+    }
   }
 }
 </style>
